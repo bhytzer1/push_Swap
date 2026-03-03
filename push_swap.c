db4f9eefafd89f0d0849ec710ad3f27b5cceea85
bhytzer1 <davidemandricardo@tuta.com>
2026-03-03 22:01:31 +0100

    Add files via upload

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dmandric <dmandric@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 11:03:47 by dmandric          #+#    #+#             */
-/*   Updated: 2026/03/03 19:28:12 by dmandric         ###   ########.fr       */
+/*   Updated: 2026/03/03 21:59:44 by dmandric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,19 @@ void append_node (t_node	**stack, t_node	*new_node)
 {
 	t_node	*last_node;
 
-	if (last_node == NULL)
-		return (0);
+	if (new_node == NULL)
+		return;
 	if (*stack == NULL)
-		*stack = *new_node;
-		return (0);
+	{
+		*stack = new_node;
+		return;
+	}
 	last_node  = *stack;
-			while (last_node-> !=)
-			{
-
-			}
+	while (last_node->next != NULL)
+	{
+		last_node = last_node->next;
+	}
+	last_node->next = new_node;
+	new_node->prev = last_node;
+	new_node->next = NULL;
 }
